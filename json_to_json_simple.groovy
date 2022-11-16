49e2c5810303dbd79781d1dc6a97c76e6ba2c7c2
macason <rick@macason.org>
2022-11-16 13:35:18 +1100

    Update json_to_json_simple.groovy

--- json_to_json_simple.groovy
+++ json_to_json_simple.groovy
@@ -45,94 +45,56 @@ import groovy.json.JsonBuilder;
 SAMPLE PAYLOAD FOR ABOVE MAPPING
 ********************************
 {
-
-        "order_event": {
-
-        "external_order_id": "DEV-ORDER-A004",
-
-        "type": "shipping",
-
-        "created": "2022-11-11T05:19:49Z",
-
-        "payload": {
-
-            "tracking_number": "PPXNrswokcWpd",
-
-            "expected_delivery_date": null,
-
-            "tracking_url": "https://app.staging.shippit.com/tracking/ppxnrswokcwpd",
-
-            "current_state": "ready_for_pickup",
-
-            "retailer_order_number": "DEV-ORDER-A004",
-
-            "retailer_reference": null,
-
-            "courier_name": "Allied Express",
-
-            "courier_job_id": "SHP132086116",
-
-            "delivery_address": "1 Union Street",
-
-            "delivery_suburb": "Pyrmont",
-
-            "delivery_postcode": "2009",
-
-            "delivery_state": "NSW",
-
-            "delivery_country_code": "AU",
-
-            "merchant_url": "",
-
-            "delivery_latitude": -33.869781,
-
-            "delivery_longitude": 151.194633,
-
-            "status_history": [{
-
-                "status": "ready_for_pickup",
-
-                "time": "2022-11-11T05:18:45+00:00"
-
-            }, {
-
-                "status": "despatch_in_progress",
-
-                "time": "2022-11-01T12:22:23+00:00"
-
-            }, {
-
-                "status": "order_placed",
-
-                "time": "2022-10-17T23:12:33+00:00"
-
-            }],
-
-            "products": [{
-
-                "title": "SKIN PROUD Everything",
-
-                "sku": "73230",
-
-                "quantity": 2,
-
-                "product_line_id": null
-
-            }, {
-
-                "title": "A BIT HIPPY Moisturiser",
-
-                "sku": "75133",
-
-                "quantity": 4,
-
-                "product_line_id": null
-
-            }]
-
-        }
-
-    }
-
+   "order_event":{
+      "external_order_id":"DEV-ORDER-A004",
+      "type":"shipping",
+      "created":"2022-11-11T05:19:49Z",
+      "payload":{
+         "tracking_number":"PPXNrswokcWpd",
+         "expected_delivery_date":null,
+         "tracking_url":"https://app.staging.shippit.com/tracking/ppxnrswokcwpd",
+         "current_state":"ready_for_pickup",
+         "retailer_order_number":"DEV-ORDER-A004",
+         "retailer_reference":null,
+         "courier_name":"Allied Express",
+         "courier_job_id":"SHP132086116",
+         "delivery_address":"1 Union Street",
+         "delivery_suburb":"Pyrmont",
+         "delivery_postcode":"2009",
+         "delivery_state":"NSW",
+         "delivery_country_code":"AU",
+         "merchant_url":"",
+         "delivery_latitude":-33.869781,
+         "delivery_longitude":151.194633,
+         "status_history":[
+            {
+               "status":"ready_for_pickup",
+               "time":"2022-11-11T05:18:45+00:00"
+            },
+            {
+               "status":"despatch_in_progress",
+               "time":"2022-11-01T12:22:23+00:00"
+            },
+            {
+               "status":"order_placed",
+               "time":"2022-10-17T23:12:33+00:00"
+            }
+         ],
+         "products":[
+            {
+               "title":"SKIN PROUD Everything",
+               "sku":"73230",
+               "quantity":2,
+               "product_line_id":null
+            },
+            {
+               "title":"A BIT HIPPY Moisturiser",
+               "sku":"75133",
+               "quantity":4,
+               "product_line_id":null
+            }
+         ]
+      }
+   }
 }
 **/
